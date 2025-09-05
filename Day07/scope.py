2870ffdabcd8efb72a7ff729ba4d96cc6235ee31
AbhayThakre <abhaythakre532@gmail.com>
2025-09-06 01:17:37 +0530

    update

--- Day07/scope.py
+++ Day07/scope.py
@@ -9,41 +9,42 @@ print(username)
 func()
 
 
-# x = 99 
-# def func2(y):
-#     z = x + y
-#     return z
+x = 99 
+def func2(y):
+    z = x + y
+    return z
 
-# result = func2(1)
-# print(result)
+result = func2(1)
+print(result)
 
-# def func3():
-#     global x
-#     x = 12
+def func3():
+    global x
+    x = 12
+    print(x)
     
-# func3()
-# print(x)
+func3()
+print(x)
 
 
 
-# def f1():
-#     x = 88
-#     def f2():
-#         print(x)
-#     return f2
-# myResult = f1()
-# myResult()
+def f1():
+    x = 88
+    def f2():
+        print(x)
+    return f2
+myResult = f1()
+myResult()
 
 
-# def chaicoder(num):
-#     def actual(x):
-#         return x ** num
-#     return actual
+def chaicoder(num):
+    def actual(x):
+        return x ** num
+    return actual
 
 
 
-# f = chaicoder(2)
-# g = chaicoder(3)
+f = chaicoder(2)
+g = chaicoder(3)
 
-# print(f(3))
-# print(g(3))
+print(f(3))
+print(g(3))
